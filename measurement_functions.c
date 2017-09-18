8986232b62c520e23f757e13fe23994b81aae9d9
caginbaris <31400655+caginbaris@users.noreply.github.com>
2017-09-18 18:35:32 +0300

    fund rms (fir implementation added)

--- measurement_functions.c
+++ measurement_functions.c
@@ -1,5 +1,5 @@
-
-// True RMS for distorted data
+// function-1
+// True RMS half cycle
 // delayLineArray contains half period circular data of input
 // delayLineCounter global counter for true rms calculation
 // length of delayLineArray - mult. inverse can be u
@@ -9,7 +9,6 @@
 float true_rms(float rtInput, float *delayLineArray, unsigned int delayLineCounter, unsigned int arrayLength)
 {
 
-	
 
 	unsigned int i;
 	float rms = 0, rms_sum = 0, rms_data=0;
@@ -32,3 +31,40 @@ float true_rms(float rtInput, float *delayLineArray, unsigned int delayLineCount
 	return rms;
 
 }
+
+
+
+// function-2
+// fundamental RMS full cycle
+
+float fundamental_rms(float rtInput,float *coeff, unsigned int coeffLength, float *zValues){
+
+	unsigned int i;
+     float *z1_ptr,*z2_ptr,*coeff_ptr;
+     float output;
+
+	z1_ptr=zValues; 		//background data
+	z2_ptr=z1_ptr; 		//data update
+	coeff_ptr=coeff+coeffLength-1;  //last element
+
+
+	output=(*z1_ptr++) *(*coeff_ptr--);
+
+	for(i=2;i<coeffLength;i++){
+	
+	*z2_ptr++ =*z1_ptr;
+	output+=(*z1_ptr++) *(*coeff_ptr--);
+
+	}
+
+
+	output+=rtInput *(*coeff_ptr);
+	*z2_ptr=rtInput;
+
+	return(output);
+
+}
+
+
+
+
--- measurement_functions.h
+++ measurement_functions.h
@@ -1,1 +1,2 @@
 float true_rms(float rtInput, float *delayLineArray, unsigned int delayLineCounter, unsigned int arrayLength);
+float fundamental_rms(float rtInput,float *coeff, unsigned int coeffLength, float *zValues);
